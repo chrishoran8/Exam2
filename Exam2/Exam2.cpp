33bdf3b3ca9e6c9109fd06dd4cec8e4f69f6a9c5
chrishoran8 <horan_chris@hotmail.co.uk>
2021-03-29 09:39:00 +0100

    tested ready for students

--- Exam2/Exam2.cpp
+++ Exam2/Exam2.cpp
@@ -13,54 +13,41 @@
 // \\192.168.68.114\test\regiNumber
 
 
-#include "functions.h";
+#include "functions.h"
 #include <iostream>
 using namespace std;
 
 int main()
-{    
+{
     //1) declare a type float called fltOne (2)
 
 
     //2) declare a type float called fltTwo (2)
 
-
     //3) declare a type float called fltThree (2)
-
 
     //4) declare type string called stringOne (2)
 
-
     //5) assign fltOne the value of 3 (2)
-
 
     //6) assign fltTwo the value of 5 (2)
 
-
     //7) assign stringOne the value of "hello"  (2)
-
 
     //8) use the multiplication operator to multiply fltOne with fltTwo 
     //use fltThree to store the result (3)
 
-
     //9) increment fltThree  (3)
-
 
     //10) initialise a type char called myChar to the value 'b' (2)
 
-
     //11) output the value fltThree and myChar to the screen  (3)
-
 
     //12) declare a variable of type int called myValue  (3)
 
-
     //13)  prompt the user to provide an input to myValue (2)    
 
-
     //14) after prompting, get a variable from the user of type int called myValue. (3) 
-
 
     //15) take the input and apply a decision based on the number inputted . 
     //The decision should be based on if the user inputs a number below 100 
@@ -69,11 +56,10 @@ int main()
 
     //16) create an array of type int with 12 elements called arrayOfInts (8)
 
-
     //17 part a) create a for loop to iterate through the above array and populate the array with 
-    //{0,3,6,9,12,15,18,21,24,27} 
+    //{0,3,6,9,12,15,18,21,24,27,30,33} 
     //(6 marks)
-
+ 
 
     //17 part b) create a for loop to iterate through the above array and output its contents
     //ensure each number is shown on the same line, but a space separates them all (6 marks)
@@ -81,12 +67,10 @@ int main()
 
     //18) initialise a variable of type int called cost to 200 (2) 
 
-
     //19) create a pointer pointing to the above variable called costPtr (4) 
 
-
     //20) create a function which calculates an increase of a given percentage (13)
-    // 
+    // the function should be called calcPerc
     //The function should be declared in the correct file and defined in the correct file
     //the function should take a cost parameter and a percentage parameter
     //it should return the cost plus the percentage increas. 
@@ -97,7 +81,7 @@ int main()
 
 
     //21) using the below string
-    string s1 = "Hello, World!";      
+    string s1 = "Hello, World!";
     //using a string method, create two separate strings from the above called s2 and s3 (5) 
     //s2 must equal "Hello," which must be taken from s1
     //s3 must equal "World!" which must be taken from s1
@@ -107,11 +91,10 @@ int main()
     //assign to lengthFG. Output the value to the console (5) 
     int lengthFG;
 
-
-
     //23) use a string method to compare  s2 and s3(5) 
-    //if the strings are "equal" according to the string method, they should output "strings equal"
-    //because we can see that they are not equal, "strings equal" should not be outputted. 
+    //if the strings are not equal according to the string method, they should output "strings not equal"
+    //because we can see that they are not equal, "strings not equal" should not be outputted. 
+
 
 
     //24) using a different string method to what you have used in above code
